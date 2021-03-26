3d406685771368af8b0140d443079f402e7b5f80
17sanjay <kvsanjay17@gmail.com>
2021-03-26 10:57:23 +0000

    done

--- Program-1.c
+++ Program-1.c
@@ -11,9 +11,20 @@ Complete the given program with string declaration/initialization & read/print t
 #include <stdio.h>
 int main() 
 {
+  int c=0,count=0;
+  char s[100];
                       //Declare the string
-printf("Enter a String: ")
-                      //read the string from user
+printf("Enter a String: ");
+scanf("%s",s);
+while (s[c] != '\0')
+{
+  if(s[c]=='a' || s[c]=='A' || s[c]='e' || s[c]=='E'
+  || s[c]=='i' || s[c]=='I' || s[c]=='o' ||  s[c]=='O'||  s[c]=='0' || s[c]=='u' ||  s[c]=='U' )
+  count++;
+  c++;
+
+}
+ printf("number of vowels i the string ="%d",count)                  
                       //Print the string
 return 0;
 }
